5ea33ad8bb719e6749dabd08a736c6e3d23e522a
ClaytonDev-F <fernandaflauzino1333@gmail.com>
2025-02-18 10:13:19 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -70,14 +70,14 @@ export default function Home() {
     const { open } = useAppKit();
     const { disconnect } = useDisconnect();
     const [bnbPrice, setBnbPrice] = useState(0);
-    const [usdRaised, setUsdRaised] = useState("56100"); //valor estático por enquanto
+    const [usdRaised, setUsdRaised] = useState("58060"); //valor estático por enquanto
 
     const percentage = (parseFloat(usdRaised) / MAX_USD_RAISED) * 100;
     //@ts-ignore
     const formatPercentage = (percentage) => `${percentage.toFixed(2)}%`;
     //@ts-ignore
     const formatNumber = (number) => Number(number).toLocaleString('en-US');
-    const tokensSold = 56100000; //valor estatico
+    const tokensSold = 58060000; //valor estatico
 
     const fetchBnbPrice = async () => {
         try {
